52cbc072d3cc2f8a00595cbaf91c9980572e2b67
Panwar, Himanshu <himanshu.panwar@boeing.com>
2024-12-20 23:23:30 +0530

    Building Portfolio viewer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PortfolioBuilder';
+  nm = '';
+  em = '';
+  ph = '';
+  add = '';
+  submitted : boolean = false;
+  showHeading : boolean = true;
+  qualification = [{school : '', degree: '', year: ''}];
+
+  addQualification()
+  {
+    this.qualification.push({school:'', degree:'',year:''});
+  }
+
+  submit()
+  {
+    this.submitted = true;
+    this.showHeading = false;
+  }
+
+  edit()
+  {
+    this.submitted = false;
+  }
 }
